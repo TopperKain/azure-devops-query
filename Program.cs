6a69fc8b37ac5c95503b36c6770a7d4f4960ac7e
Topper Kain <topkain@microsoft.com>
2020-02-11 19:24:58 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WitQuickStarts.Samples;
+
+namespace WitQuickStarts
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            //if (args.Length == 0)
+            //{
+            //    ShowUsage();
+            //    return 0;
+            //}
+
+            string connectionUrl, token, repoId = "";
+
+            try
+            {
+                CheckArguments(args, out connectionUrl, out token, out repoId);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                ShowUsage();
+
+                return -1;
+            }
+
+            try
+            {
+                Console.WriteLine("Executing quick start samples...");
+                Console.WriteLine("");
+
+                //todo: may need to adjust this to scale better for more samples.
+
+                ////instantiate objects
+                //Samples.CreateBug objBug = new Samples.CreateBug(connectionUrl, token, repoId);
+                //Samples.ExecuteQuery objQuery = new Samples.ExecuteQuery(connectionUrl, token, repoId);
+                var objPrsByWorkItem = new GetPullRequestsWithWorkItemInfo(connectionUrl, token, repoId);
+                objPrsByWorkItem.GetPRsWithWorkItemInfo().Wait();
+
+
+                ////execute the client lib code. If you want to run the direct http calls then adjust (see below)
+                //objBug.CreateBugUsingClientLib();
+                //objQuery.RunGetBugsQueryUsingClientLib();
+
+                //objBug.CreateBugUsingHTTP();
+                //objQuery.RunGetBugsQueryUsingHTTP();
+
+                //objBug = null;
+                //objQuery = null;
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to run the sample: " + ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Runs the WIT Quick Start samples on a Team Services account or Team Foundation Server instance.");
+            Console.WriteLine("");
+            Console.WriteLine("These samples are to provide you the building blocks of using the REST API's in Work Item Tracking.");
+            Console.WriteLine("Examples are written using the .NET client library and using direct HTTP calls. We recommend, that");
+            Console.WriteLine("whenever possible, you use the .NET client library.");
+            Console.WriteLine("");
+            Console.WriteLine("!!WARNING!! Some samples are destructive. Always run on a test account or collection.");
+            Console.WriteLine("");
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("");
+            Console.WriteLine("  /url:fabrikam.visualstudio.com /token:personalaccesstoken /repoId:repoGuid");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+
+        private static void CheckArguments(string[] args, out string connectionUrl, out string token, out string repoId)
+        {
+            connectionUrl = null;
+            token = null;
+            repoId = null;
+            
+            Dictionary<string, string> argsMap = new Dictionary<string, string>();
+            foreach (var arg in args)
+            {
+                if (arg[0] == '/' && arg.IndexOf(':') > 1)
+                {
+                    string key = arg.Substring(1, arg.IndexOf(':') - 1);
+                    string value = arg.Substring(arg.IndexOf(':') + 1);
+
+                    switch (key)
+                    {
+                        case "url":
+                            connectionUrl = value;
+                            break;
+
+                        case "token":
+                            token = value;                          
+                            break;
+
+                        case "repoId":
+                            repoId = value;
+                            break;
+                        default:
+                            throw new ArgumentException("Unknown argument", key);
+                    }
+                }
+            }
+
+            if (connectionUrl == null)
+            {
+                Console.WriteLine("Provide Connection URL");
+                connectionUrl = Console.ReadLine();
+            }
+
+            if (token == null)
+            {
+
+                Console.WriteLine("Provide Personal Access Token (PAT)");
+                token = Console.ReadLine();
+            }
+
+            if (repoId == null)
+            {
+
+                Console.WriteLine("Provide repoId)");
+                repoId = Console.ReadLine();
+            }
+
+            if (string.IsNullOrWhiteSpace(connectionUrl) || string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentException("Missing required arguments");
+            }
+        }
+
+
+    }
+}
---
+++ Samples/GetPullRequestsWithWorkItemInfo.cs
@@ -0,0 +1,198 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.CircuitBreaker;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Location;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WitQuickStarts.Samples
+{
+    class GetPullRequestsWithWorkItemInfo
+    {
+        readonly string _uri;
+        readonly string _personalAccessToken;
+        readonly string repoId;
+
+        public GetPullRequestsWithWorkItemInfo(string url, string pat, string project)
+        {
+            _uri = url;
+            _personalAccessToken = pat;
+            repoId = project;
+        }
+
+
+        public async Task GetPRsWithWorkItemInfo()
+        {
+            try
+            {
+                Uri uri = new Uri(_uri);
+                string personalAccessToken = _personalAccessToken;
+                Guid repo = new Guid(repoId);
+
+                VssBasicCredential credentials = new VssBasicCredential("", _personalAccessToken);
+
+                //create instance of work item tracking http client
+                using (WorkItemTrackingHttpClient workItemTrackingHttpClient = new WorkItemTrackingHttpClient(uri, credentials))
+                using (GitHttpClient gitClient = new GitHttpClient(uri, credentials))
+                {
+                    List<GitPullRequest> prs = await gitClient.GetPullRequestsAsync(repo, new GitPullRequestSearchCriteria() { Status = PullRequestStatus.Completed });
+                    var recentPrs = prs.Where(pr => pr.ClosedDate > DateTime.Now.AddDays(-90));
+
+                    int prCount = recentPrs.Count();
+                    int currentPr = 0;
+                    List<PullRequestFractional> pullRequests = new List<PullRequestFractional>();
+                    foreach (var pr in recentPrs)
+                    {
+                        currentPr++;
+                        Console.WriteLine($"Working n {currentPr} of {prCount} pull requests");
+                        var workItemRefs = await gitClient.GetPullRequestWorkItemRefsAsync(repo, pr.PullRequestId);
+                        var workItemIds = workItemRefs
+                            .Where(wiRef => int.TryParse(wiRef.Id, out int throwaway))
+                            .Select(wiRef => int.Parse(wiRef.Id));
+                        var createdBy = pr.CreatedBy.DisplayName;
+                        var name = pr.Title.Replace(',', ' ');
+                        var id = pr.PullRequestId;
+                        PullRequestFractional pullRequest = new PullRequestFractional()
+                        {
+                            name = name,
+                            id = id,
+                            createdBy = createdBy,
+                            workItems = new List<WorkItemFractional>(),
+                            totalBugs = 0,
+                            totalStoryPoints = 0,
+                            totalUserStories = 0,
+                            totalWorkItems = 0
+                        };
+
+                        pullRequests.Add(pullRequest);
+
+                        foreach (var workItemRef in workItemIds)
+                        {
+                            var workItem = await workItemTrackingHttpClient.GetWorkItemAsync(workItemRef);
+                            string storyPoints = string.Empty;
+                            string workItemType = string.Empty;
+                            string workItemTitle = string.Empty;
+                            if (workItem.Fields.ContainsKey("Microsoft.VSTS.Scheduling.StoryPoints")){
+                                storyPoints = workItem.Fields["Microsoft.VSTS.Scheduling.StoryPoints"]?.ToString();
+                            }
+                            if (workItem.Fields.ContainsKey("System.WorkItemType"))
+                            {
+                                workItemType = workItem.Fields["System.WorkItemType"]?.ToString();
+                            }
+                            if (workItem.Fields.ContainsKey("System.Title"))
+                            {
+                                workItemTitle = workItem.Fields["System.Title"]?.ToString();
+                            }
+
+                            var workItemFrac = new WorkItemFractional()
+                            {
+                                id = workItemRef,
+                                workItemType = workItemType,
+                                workItemTitle = workItemTitle,
+                                storyPoints = string.IsNullOrEmpty(storyPoints) ? 0 : double.Parse(storyPoints)
+
+                            };
+
+                            switch (workItemFrac.workItemType.ToUpperInvariant())
+                            {
+                                case "USER STORY":
+                                    pullRequest.totalWorkItems++;
+                                    pullRequest.totalUserStories++;
+                                    break;
+                                case "BUG":
+                                    pullRequest.totalWorkItems++;
+                                    pullRequest.totalBugs++;
+                                    break;
+                            }
+
+                            pullRequest.totalStoryPoints += workItemFrac.storyPoints;
+                            pullRequest.dividedStoryPoints = 0;
+                            pullRequest.workItems.Add(workItemFrac);
+                        }
+                    }
+
+                    // Add up the divided story points (when the  work item is associated with multiple PRs, don't double count the points- divide them equally between PRs)
+                    var distinctWorkItems = pullRequests
+                        .SelectMany(pr => pr.workItems)
+                        .Where(workItem => workItem.storyPoints > 0)
+                        .Select(workItem => new { workItem.id, workItem.storyPoints })
+                        .Distinct();
+
+                    foreach (var workItem in distinctWorkItems)
+                    {
+                        var prsAssociatedWithWorkItem = pullRequests.Where(pr => pr.workItems.Any(wi => wi.id == workItem.id));
+                        double associatedPrCount = (double)prsAssociatedWithWorkItem.Count();
+                        double storyPoints = workItem.storyPoints;
+                        double dividedPoints = storyPoints / associatedPrCount;
+
+                        foreach (var assoicatedPr in prsAssociatedWithWorkItem)
+                        {
+                            assoicatedPr.dividedStoryPoints += dividedPoints;
+                        }
+                    }
+
+                    Console.WriteLine("finishing");
+
+                    var memStream = new MemoryStream();
+                    
+                    using (var consoleWriter = new StreamWriter(memStream))
+                    {
+                        var csv = new CsvWriter(consoleWriter, new CsvConfiguration(CultureInfo.InvariantCulture));
+                        csv.WriteRecords(pullRequests);
+                        consoleWriter.Flush();
+                    }
+
+                    memStream.Flush();
+                    byte[] byteArray = memStream.ToArray();
+                    string output1 = Encoding.ASCII.GetString(byteArray, 0, byteArray.Length);
+                    Console.WriteLine(output1);
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public class WorkItemFractional : IEquatable<WorkItemFractional>, IComparable<WorkItemFractional>
+        {
+            public int id { get; set; }
+            public string workItemType { get; set; }
+            public double storyPoints { get; set; }
+            public string workItemTitle { get; set; }
+
+            public int CompareTo(WorkItemFractional other)
+            {
+                return this.id.CompareTo(other.id);
+            }
+
+            public bool Equals(WorkItemFractional other)
+            {
+                return id == other.id;
+            }
+        }
+
+        public class PullRequestFractional
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string createdBy { get; set; }
+            public double totalStoryPoints { get; set; }
+            public int totalWorkItems { get; set; } 
+            public int totalBugs { get; set; }
+            public int totalUserStories { get; set; }
+            public double dividedStoryPoints { get; set; }
+
+            public List<WorkItemFractional> workItems { get; set; }
+        }
+    }
+}
